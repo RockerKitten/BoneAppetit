ce92e6f9c428d51cb5c15bdfd7b76961e37212b9
RockerKitten <dragon.child569@gmail.com>
2021-05-25 11:38:30 -0500

    re linking git
    Signed-off-by: RockerKitten <dragon.child569@gmail.com>

---
+++ JotunnModStub/BoneAppetit.cs
@@ -0,0 +1,182 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+using System.Reflection;
+using Jotunn.Entities;
+using Jotunn.Configs;
+using Jotunn.Managers;
+using System;
+
+namespace Grills
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    internal class Grills : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.rockerkitten.boneappetit";
+        public const string PluginName = "BoneAppetit";
+        public const string PluginVersion = "1.0.0";
+        public AssetBundle assetBundle;
+        private AssetBundle customfood;
+        private void Awake()
+        {
+            AssetLoad();
+            LoadItem();
+            LoadGriddle();
+            IceCream();
+            PorkRind();
+            Kabob();
+            FriedLox();
+            GlazedCarrot();
+            
+        }
+
+        private void AssetLoad()
+        {
+            assetBundle = AssetUtils.LoadAssetBundleFromResources("grill", typeof(Grills).Assembly);
+            customfood = AssetUtils.LoadAssetBundleFromResources("customfood", Assembly.GetExecutingAssembly());
+            
+        }
+       
+        private void LoadItem()
+        {
+            //piece_grill
+
+            var grillfab = assetBundle.LoadAsset<GameObject>("rk_grill");
+            var grill = new CustomPiece(grillfab,
+                new PieceConfig
+                {
+                    CraftingStation = "forge",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Stone", Amount = 10, Recover = true },
+                        new RequirementConfig { Item = "Iron", Amount = 1, Recover = true }
+                    }
+                });
+            PieceManager.Instance.AddPiece(grill);
+        }
+        private void LoadGriddle()
+        {
+            //piece_griddle
+
+            var griddlefab = assetBundle.LoadAsset<GameObject>("rk_griddle");
+            var griddle = new CustomPiece(griddlefab,
+                new PieceConfig
+                {
+                    CraftingStation = "",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Stone", Amount = 10, Recover = true }
+                    }
+                });
+            PieceManager.Instance.AddPiece(griddle);
+        }
+        private void IceCream()
+        {
+            var icecream_prefab = customfood.LoadAsset<GameObject>("rk_icecream");
+            var icecream = new CustomItem(icecream_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Ice Cream",
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "FreezeGland", Amount = 4},
+                        new RequirementConfig { Item = "Blueberries", Amount = 8},
+                        new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(icecream);
+
+        }
+        private void PorkRind()
+        {
+            var porkrind_prefab = customfood.LoadAsset<GameObject>("rk_porkrind");
+            var porkrind = new CustomItem(porkrind_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Pork Rinds",
+                    Amount = 2,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "LeatherScraps", Amount = 1}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(porkrind);
+
+        }
+        private void Kabob()
+        {
+            var kabob_prefab = customfood.LoadAsset<GameObject>("rk_kabob");
+            var kabob = new CustomItem(kabob_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Kabob",
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Turnip", Amount = 1},
+                        new RequirementConfig { Item = "Carrot", Amount = 2},
+                        new RequirementConfig { Item = "RawMeat", Amount = 1},
+                        new RequirementConfig { Item = "BoneFragments", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(kabob);
+
+        }
+        private void FriedLox()
+        {
+            var friedlox_prefab = customfood.LoadAsset<GameObject>("rk_friedloxmeat");
+            var friedlox = new CustomItem(friedlox_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Country Fried Lox Meat",
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "LoxMeat", Amount = 2},
+                        new RequirementConfig { Item = "BarleyFlour", Amount = 2},
+                        new RequirementConfig { Item = "Thistle", Amount = 1},
+                        new RequirementConfig { Item = "Cloudberry", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(friedlox);
+
+        }
+        private void GlazedCarrot()
+        {
+            var glazedcarrot_prefab = customfood.LoadAsset<GameObject>("rk_glazedcarrots");
+            var glazedcarrot = new CustomItem(glazedcarrot_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Honey Glazed Carrots",
+                    Amount = 1,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Carrot", Amount = 2},
+                        new RequirementConfig { Item = "Honey", Amount = 2},
+                        new RequirementConfig { Item = "Dandelion", Amount = 1 }
+                    }
+                });
+
+            ItemManager.Instance.AddItem(glazedcarrot);
+
+        }
+    }
+}
---
+++ JotunnModStubPackage/README.md
@@ -0,0 +1,57 @@
+**MOD Description** 
+
+Master Chef 2.0 is a partial remake of Master Chef with 3d models by deBARBA
+Please support his artwork he made some really amazing models for this 
+
+You can find him here:
+patreon.com/littleroomdev 
+
+
+I have permission from hrve to remake master chef and can furnish proof if needed 
+
+
+You will get 6 new food items with this mod and 1 new wearable item
+
+Burger
+Blood Sausage
+Broth
+Carrot Butter
+Carrot Butter Jelly Sandwich
+Dragon Omlette
+Fish Stew
+
+
+and finally a Chef Hat as a wearable item. 
+
+
+---
+**Installation**
+
+1) Go to [BepInExPack Valheim](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
+2) Download it and follow the installation manual
+3) Drag the unzipped MasterChef20 folder into -> <Steam Location>\steamapps\common\Valheim\BepInEx\plugin\  
+
+The next time you start your game, the mod will be enabled. To disable it, just remove it from the mods folder.
+
+---
+**Usage**
+
+Recipes should appear in game through normal play. If you are installing this mod on an existing playthrough, you will have to pick up items or re-order you inventory to learn the new recipes.
+
+---
+I wanna extend a huge thank you to Jotunn team they are amazing and basically enabled me to make this project :D 
+
+--- 
+**Changelog**
+
+**v1.0.0**
+
+* initial release 
+
+**v1.0.1**
+
+* readme updates
+
+**v1.0.2**
+
+* readme updates
