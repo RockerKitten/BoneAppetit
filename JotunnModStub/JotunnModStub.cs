314fe959ad34eaa2e2173603d2eb5088849ddee1
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-06-01 18:42:04 -0500

    Initial commit

---
+++ JotunnModStub/JotunnModStub.cs
@@ -0,0 +1,42 @@
+﻿// JotunnModStub
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    JotunnModStub.cs
+// Project: JotunnModStub
+
+using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+
+namespace JotunnModStub
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class JotunnModStub : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.jotunn.jotunnmodstub";
+        public const string PluginName = "JotunnModStub";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+            // Do all your init stuff here
+            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
+            Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("ModStub has landed");
+        }
+
+#if DEBUG
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F6))
+            { // Set a breakpoint here to break on F6 key press
+            }
+        }
+#endif
+    }
+}
