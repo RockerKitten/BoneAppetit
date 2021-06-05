4135dcd1919dc5dc85745f1437b774d13dbf86f2
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-06-05 00:16:33 -0500

    attempting to fix all the things

---
+++ JotunnModStub/BoneAppetit.cs
@@ -0,0 +1,473 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+using System.Reflection;
+using Jotunn.Entities;
+using Jotunn.Configs;
+using Jotunn.Managers;
+using System;
+
+namespace BoneAppetit
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    internal class BoneAppetit : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.rockerkitten.boneappetit";
+        public const string PluginName = "BoneAppetit";
+        public const string PluginVersion = "1.2.0";
+        private AssetBundle assetBundle;
+        private AssetBundle customfood;
+        
+
+        private void Awake()
+        {
+
+            AssetLoad();
+            //AddSkills();
+            LoadItem();
+            LoadGriddle();
+            Oven();
+            IceCream();
+            PorkRind();
+            Kabob();
+            FriedLox();
+            GlazedCarrot();
+            Bacon();
+            SmokedFish();
+            Pancakes();
+            Pizza();
+            Coffee();
+            Latte();
+            FireCream();
+            ElectricCream();
+            AcidCream();
+            Porridge();
+            //Cake();
+            //PBJ();
+
+
+        }
+        private void AssetLoad()
+        {
+            assetBundle = AssetUtils.LoadAssetBundleFromResources("grill", Assembly.GetExecutingAssembly());
+            customfood = AssetUtils.LoadAssetBundleFromResources("customfood", Assembly.GetExecutingAssembly());
+            //CookingSprite = customfood.LoadAsset<Sprite>("rkcookingsprite");
+        }
+        
+        private void LoadItem()
+        {
+            //piece_grill
+
+            var grillfab = assetBundle.LoadAsset<GameObject>("rk_grill");
+            var grill = new CustomPiece(grillfab,
+                new PieceConfig
+                {
+                    CraftingStation = "forge",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Stone", Amount = 10, Recover = true },
+                        new RequirementConfig { Item = "Iron", Amount = 1, Recover = true }
+                    }
+                });
+            PieceManager.Instance.AddPiece(grill);
+        }
+        
+        private void LoadGriddle()
+        {
+            //piece_griddle
+
+            var griddlefab = assetBundle.LoadAsset<GameObject>("rk_griddle");
+            var griddle = new CustomPiece(griddlefab,
+                new PieceConfig
+                {
+                    CraftingStation = "",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Stone", Amount = 10, Recover = true }
+                    }
+                });
+            PieceManager.Instance.AddPiece(griddle);
+        }
+        private void Oven()
+        {
+            //piece_griddle
+
+            var ovenfab = assetBundle.LoadAsset<GameObject>("rk_oven");
+            var oven = new CustomPiece(ovenfab,
+                new PieceConfig
+                {
+                    CraftingStation = "",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "SurtlingCore", Amount = 2, Recover = true },
+                        new RequirementConfig { Item = "TrophySurtling", Amount = 1, Recover = true },
+                        new RequirementConfig { Item = "Stone", Amount = 10, Recover = true }
+                    }
+                });
+            PieceManager.Instance.AddPiece(oven);
+        }
+        private void IceCream()
+        {
+            var icecream_prefab = customfood.LoadAsset<GameObject>("rk_icecream");
+            var icecream = new CustomItem(icecream_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Ice Cream",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "FreezeGland", Amount = 4},
+                        new RequirementConfig { Item = "Blueberries", Amount = 8},
+                        new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(icecream);
+
+        }
+        private void PorkRind()
+        {
+            var porkrind_prefab = customfood.LoadAsset<GameObject>("rk_porkrind");
+            var porkrind = new CustomItem(porkrind_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Pork Rinds",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "LeatherScraps", Amount = 1}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(porkrind);
+
+        }
+        private void Kabob()
+        {
+            var kabob_prefab = customfood.LoadAsset<GameObject>("rk_kabob");
+            var kabob = new CustomItem(kabob_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Kabob",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Turnip", Amount = 1},
+                        new RequirementConfig { Item = "Carrot", Amount = 2},
+                        new RequirementConfig { Item = "RawMeat", Amount = 1},
+                        new RequirementConfig { Item = "BoneFragments", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(kabob);
+
+        }
+        private void FriedLox()
+        {
+            var friedlox_prefab = customfood.LoadAsset<GameObject>("rk_friedloxmeat");
+            var friedlox = new CustomItem(friedlox_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Chicken Fried Lox Meat",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "LoxMeat", Amount = 2},
+                        new RequirementConfig { Item = "BarleyFlour", Amount = 2},
+                        new RequirementConfig { Item = "Thistle", Amount = 1},
+                        new RequirementConfig { Item = "Cloudberry", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(friedlox);
+
+        }
+        private void GlazedCarrot()
+        {
+            var glazedcarrot_prefab = customfood.LoadAsset<GameObject>("rk_glazedcarrots");
+            var glazedcarrot = new CustomItem(glazedcarrot_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Honey Glazed Carrots",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Carrot", Amount = 3},
+                        new RequirementConfig { Item = "Honey", Amount = 2},
+                        new RequirementConfig { Item = "Dandelion", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(glazedcarrot);
+
+        }
+        private void Bacon()
+        {
+            var bacon_prefab = customfood.LoadAsset<GameObject>("rk_bacon");
+            var bacon = new CustomItem(bacon_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Bacon",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "RawMeat", Amount = 1}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(bacon);
+        }
+        private void SmokedFish()
+        {
+            var smokedfish_prefab = customfood.LoadAsset<GameObject>("rk_smokedfish");
+            var smokedfish = new CustomItem(smokedfish_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "SmokedFish",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_griddle",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "FishRaw", Amount = 1}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(smokedfish);
+
+        }
+        private void Pancakes()
+        {
+            var pancake_prefab = customfood.LoadAsset<GameObject>("rk_pancake");
+            var pancake = new CustomItem(pancake_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Pancakes",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Honey", Amount = 2},
+                        new RequirementConfig { Item = "BarleyFlour", Amount = 3},
+                        new RequirementConfig { Item = "Cloudberry", Amount = 5}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(pancake);
+
+        }
+        private void Pizza()
+        {
+            var pizza_prefab = customfood.LoadAsset<GameObject>("rk_pizza");
+            var pizza = new CustomItem(pizza_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Pizza",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Honey", Amount = 2},
+                        new RequirementConfig { Item = "BarleyFlour", Amount = 3},
+                        new RequirementConfig { Item = "Mushroom", Amount = 2},
+                        new RequirementConfig { Item = "CookedMeat", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(pizza);
+        }
+
+        private void Coffee()
+        {
+            var coffee_prefab = customfood.LoadAsset<GameObject>("rk_coffee");
+            var coffee = new CustomItem(coffee_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Coffee",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                         new RequirementConfig { Item = "AncientSeed", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(coffee);
+
+        }
+        private void Latte()
+        {
+            var latte_prefab = customfood.LoadAsset<GameObject>("rk_latte");
+            var latte = new CustomItem(latte_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Spice Latte",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                         new RequirementConfig { Item = "Crystal", Amount = 2},
+                         new RequirementConfig { Item = "Barley", Amount = 2},
+                         new RequirementConfig { Item = "Honey", Amount = 10}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(latte);
+
+        }
+        private void FireCream()
+        {
+            var firecream_prefab = customfood.LoadAsset<GameObject>("rk_firecream");
+            var firecream = new CustomItem(firecream_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Fire Cream",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "SurtlingCore", Amount = 4},
+                        new RequirementConfig { Item = "Raspberry", Amount = 8},
+                        new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(firecream);
+
+        }
+        private void ElectricCream()
+        {
+            var electriccream_prefab = customfood.LoadAsset<GameObject>("rk_electriccream");
+            var electriccream = new CustomItem(electriccream_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Electric Cream",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Crystal", Amount = 4},
+                        new RequirementConfig { Item = "Cloudberry", Amount = 8},
+                        new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(electriccream);
+
+        }
+        private void AcidCream()
+        {
+            var acidcream_prefab = customfood.LoadAsset<GameObject>("rk_acidcream");
+            var acidcream = new CustomItem(acidcream_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Acid Cream Cone",
+                    Enabled = true,
+                    Amount = 2,
+                    CraftingStation = "piece_cauldron",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Guck", Amount = 4},
+                        new RequirementConfig { Item = "MushroomYellow", Amount = 8},
+                        new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(acidcream);
+
+        }
+        private void Porridge()
+        {
+            var porridge_prefab = customfood.LoadAsset<GameObject>("rk_porridge");
+            var porridge = new CustomItem(porridge_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Porridge",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                          new RequirementConfig { Item = "Barley", Amount = 2},
+                          new RequirementConfig { Item = "Cloudberry", Amount = 4},
+                          new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(porridge);
+
+        }
+        /*private void Cake()
+        {
+            var cake_prefab = customfood.LoadAsset<GameObject>("rk_birthday");
+            var cake = new CustomItem(cake_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Birthday Cake",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                          new RequirementConfig { Item = "BarleyFlour", Amount = 3},
+                          new RequirementConfig { Item = "Cloudberry", Amount = 4},
+                          new RequirementConfig { Item = "Honey", Amount = 2}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(cake);
+
+        }
+        private void PBJ()
+        {
+            var pbj_prefab = customfood.LoadAsset<GameObject>("rk_pbj");
+            var pbj = new CustomItem(pbj_prefab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "Jimmy's PBJ",
+                    Enabled = true,
+                    Amount = 4,
+                    CraftingStation = "rk_grill",
+                    Requirements = new[]
+                    {
+                          new RequirementConfig { Item = "Bread", Amount = 1},
+                          new RequirementConfig { Item = "QueensJam", Amount = 1},
+                          new RequirementConfig { Item = "Flax", Amount = 5}
+                    }
+                });
+
+            ItemManager.Instance.AddItem(pbj);
+
+        }*/
+
+    }
+}
--- JotunnModStub/Package/README.md
+++ JotunnModStub/Package/README.md
@@ -1,14 +1,63 @@
-﻿# JotunnModStub
-Upon building for release, this README.md is copied from `ProjectRoot/Package/` into `ProjectRoot/` such that the readme is displayed when the project is viewed via github. Make sure to edit the README.md inside the package directory, and not the project root as your changes will be overwritten.
+Introducing the newest fad in Viking cuisine! Hope you enjoy, Bone Appetit!
 
-## Installation (manual)
+Changelog
+v. 1.0.0 	release includes 2 food crafting stations that require a fire underneath to cook on, and 5 new food items, one for each biome.
+v. 1.0.1 	hotfix for those not using V+
+v. 1.1.0 	fix for the grill making it difficult to load the fire underneath
+			fix for Fried Lox not auto picking up
+			5 new foods. Pancakes, Smoked Fish, Bacon, Coffee, and Pizza
+v. 1.1.1	updated visuals on bacon
+			Fixed collider on Griddle
+			Fixed attach point on kabob
+v. 1.2.0	Updated Bacon Visuals (thicker) and fixed attach point
+			Added "Elemental" Cream Cones updated visuals on Ice Cream
+			Minor Rebalance pass (mainly on coffee)
+			Added Porridge
+			Added CONFIG!!! (only a true/false for enabling/disabling recipes, likely will stay this way) Server synced. 
+				-MAKE SURE YOU HAVE THE MOD AND CONFIG ON THE SERVER FOR THE SERVER SYNC TO WORK-
+					Everying defaults to on, when loading for the first time.
+			
+
+Note: this mod requires JVL (Jotunn the Valheim Lib) and it's dependent HookGenPatcher, it will not work without it.
+
+Cooking Stations
+Griddle  - buildable with 10 stone and a hammer. Allows for custom food as soon as you find a place to build it.
+Grill       - requires a forge, 1 iron and 10 stone to build. unlock higher tier grilled foods.
+
+Food included in initial release
+Pork Rinds
+Honey Glazed Carrots
+Kabobs
+Ice Cream Cone
+Country Fried Lox Meat
+
+v. 1.1.0
+Smoked Fish
+
+Food Menu Assets Courtesy of zarboz
+-Bacon
+-Coffee
+-Pizza
+-Pancakes
+
+v. 1.2.0
+Porridge
+"Elemental" cream cones
+
+new cooking station piece add on-
+Oven (requires "attaching" to the grill)
+-gives the grill +1 level
+
+To Do/ Current Ideas
+x-add food menu items (done)
+-expand menu (In progress)
+-localizaion
+-add another new processing station for food and expansions
+-add new food drops from creatures
+-meals/config sync (config done)
+
+This mod is routinely tested on a dedicated server with a great many other mods. To ensure your crafting stations don't disappear, and that food doesn't turn to dust, please put on the dedicated server as well as ALL clients.
 
 
-## Features
-
-
-## Changelog
-
-
-## Known issues
-You can find the github at:
+Huge thanks to zarboz, GraveBear, and plumga for helping me get going, setting me up, and encouraging me the whole way! This mod wouldn't exist without them.
+Also a big thanks to my "players" on my server (my husband and our good friend) Itsmesds, and JaxomFaux who've helped with ideas and balance from the start. (Bone Appetit's name came from us haning out when itmesds said how about "bone appetit" and it stuck)
