3be829c73a5c0fa4f13868c7f73b022d742c5ed1
Nicholas Dunnaway <nick@dunnaway.org>
2021-08-25 22:45:25 -0700

    Added Cooking Skill
    [Added Cooking Skill]
    Gain XP when cooking at a Cooking Station, or cooking a Consumable at the rk_griddle, rk_grill, rk_prep or piece_cauldron. Each level of cooking skill adds a 1% chance to craft an additional consumable. after lvl 25, your total cooking level is divided by 4 and the results is the % chance to crafted a 2nd additional consumable.
    
    e.g. At level 100, there is a 100% chance to craft an additional consumable and 25% chance to craft a 2nd additional consumable on top of that. At level 100 a food that crafts in stacks of x5, if lucky could output 7 total items.
    
    Set CookingSkillEnable = false to disable.
    
    [Added SE_CheffHat]
    Improves Cooking Skill XP Earned while wearing the Chef Hat. Set HatXpGain = 1 to disable.
    
    Taking the Chef Hat on and off displays a random Julia Child's quote. Displaying this message can be disabled by setting HatSEMessage to false.
    
    [Added Configs]
    - CookingSkillEnable (server synced, enforced)
    - BonusWhenCookingEnabled (server synced, enforced)
    - HatXpGain (server synced, enforced)
    - HatSEMessage (local)
    
    [Other]
    - Update Mod Version to 3.1.0
    - Updated NuGet packages
    - Added NexusId for update notice
    - Added Debug Logging for DEBUG builds
    - Added Harmony Patches for Inventory.AddItem and CookingStation.CookItem
    - Update ReadMe
    
    [ToDo]
    - Cooking Skill needs a Sprite Graphic

---
+++ JotunnModStub/SE_CheffHat.cs
@@ -0,0 +1,105 @@
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace Boneappetit
+{
+  /// <summary>
+  /// Chef Hat Status Effect.
+  /// </summary>
+  [UsedImplicitly]
+  // ReSharper disable once InconsistentNaming
+  public class SE_CheffHat : SE_Stats
+  {
+    /// <summary>
+    /// ctor
+    /// </summary>
+    public SE_CheffHat()
+    {
+      name = "rkCookingSkillStatusEffect";
+      m_name = "Cooking Bonus";
+      m_raiseSkill = BoneAppetit.Instance.rkCookingSkill;
+      m_raiseSkillModifier = BoneAppetit.Instance.HatXpGain.Value;
+      m_startMessageType = MessageHud.MessageType.Center;
+      m_stopMessageType = MessageHud.MessageType.Center;
+      SetMessages();
+    }
+
+    /// <summary>
+    /// Randomly sets the status effect start and stop message
+    /// </summary>
+    private void SetMessages()
+    {
+      if (!BoneAppetit.Instance.HatSEMessage.Value)
+      {
+        if (string.IsNullOrEmpty(m_startMessage) && string.IsNullOrEmpty(m_stopMessage)) return;
+        {
+          m_startMessage = string.Empty;
+          m_stopMessage = string.Empty;
+          return;
+        }
+      }
+
+      var current = m_startMessage;
+      do
+      {
+        m_startMessage = RandomJuliaChildPhrase.GetRandomPhrase();
+      } while (m_startMessage.Equals(m_stopMessage) || m_startMessage.Equals(current));
+
+      current = m_stopMessage;
+      do
+      {
+        m_stopMessage = RandomJuliaChildPhrase.GetRandomPhrase();
+      } while (m_stopMessage.Equals(m_startMessage) || m_stopMessage.Equals(current));
+    }
+
+    #region Overrides of SE_Stats
+
+    /// <inheritdoc />
+    public override void Setup(Character character)
+    {
+      SetMessages();
+      base.Setup(character);
+    }
+
+    #endregion
+  }
+
+  /// <summary>
+  /// Random Julia Child Phrases
+  /// </summary>
+  public static class RandomJuliaChildPhrase
+  {
+    /// <summary>
+    /// List of Julia Child Phrases
+    /// </summary>
+    private static readonly List<string> PhrasesList = new List<string>
+    {
+      "No one is born a great cook, one learns by doing. - Julia Child"
+      , "You don’t have to cook fancy or complicated masterpieces, just good food from fresh ingredients. - Julia Child"
+      , "People who love to eat are always the best people. - Julia Child"
+      , "Cooking well doesn’t mean cooking fancy. - Julia Child"
+      , "The only time to eat diet food is while you’re waiting for the steak to cook. - Julia Child"
+      , "Until I discovered cooking, I was never really interested in anything. - Julia Child"
+      , "You are the butter to my bread, and the breath to my life. - Julia Child"
+      , "Fat gives things flavor. - Julia Child"
+      , "If you’re afraid of butter, use cream. - Julia Child"
+      , "I was 32 when I started cooking; up until then, I just ate. - Julia Child"
+      , "A party without cake is just a meeting. - Julia Child"
+      , "The only real stumbling block is fear of failure. In cooking you’ve got to have a what-the-hell attitude. - Julia Child"
+      , "In France, cooking is a serious art form and a national sport. - Julia Child"
+      , "You are the BOSS of that dough. - Julia Child"
+      , "with enough butter anything is good. - Julia Child"
+      , "Usually, one’s cooking is better than one thinks it is. - Julia Child"
+    };
+
+    /// <summary>
+    /// Gets a random Julia Child Phrase.
+    /// </summary>
+    /// <returns>Random Julia Child Phrase</returns>
+    public static string GetRandomPhrase()
+    {
+      return PhrasesList[Random.Range(0, PhrasesList.Count -1)];
+    }
+  }
+}
